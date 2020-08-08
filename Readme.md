73669c16610d61560452b3bf6efcc3289b8c0f62
hanappe <hanappe@csl.sony.fr>
2020-08-08 11:27:31 +0200

    first import

--- Readme.md
+++ Readme.md
@@ -3,7 +3,38 @@ romidata2
 
 WORK IN PROGRESS
 
+
+## Installation
+
+Clone the code repository from github:
+
+```
+git clone https://github.com/romi/romidata2.git
+```
+
+
+Install the dependencies for the romidata2 module:
+
+```shell
+python -m pip install tzlocal
+python -m pip install python-dateutil
+python -m pip install fs
+```
+
+In addition, if you want to use the REST server, you must install the
+following modules:
+
+```shell
+python -m pip install flask flask_cors flask_restful
+python -m pip install Pillow
+```
+
+## Examples
+
+See examples/ for some example usages.
+
+## Documentation
+
 * See docs/webapi/ for documenation on the REST API.
 * See docs/assets/ for a diagram of the datamodel.
-* See examples/ for some example usages.
 
--- bin/romi_farmers_dashboard_api.py
+++ bin/romi_farmers_dashboard_api.py
@@ -3,13 +3,23 @@
 import sys
 from os.path import abspath
 
+import argparse
+
 sys.path.append(abspath('.'))
 from romidata2.webapp import FarmWebApp
 from romidata2.db import FarmDatabase
 from romidata2.webcache import WebCache
 
 if __name__ == "__main__":
-    db = FarmDatabase("demo/db")
-    cache = WebCache(db, "farms", "/tmp/romicache")
+    parser = argparse.ArgumentParser(description="Start the REST server")
+    parser.add_argument("-d", "--db", required=True,
+                        help="The path of the database directory")
+    parser.add_argument("-c", "--cache", required=True,
+                        help="The path to the web cache directory")
+    parser.add_argument("-t", "--type", required=True,
+                        help="Either 'farms' or 'investigations'")
+    
+    db = FarmDatabase(args.db)
+    cache = WebCache(db, args.type, args.cache)
     app = FarmWebApp(db, cache)
     app.run(host='0.0.0.0')
